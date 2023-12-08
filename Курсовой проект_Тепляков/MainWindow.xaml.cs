4915954340259f12ce79200e033ae275452e6c30
Kirill <kirillteplycov888@gmail.com>
2023-12-08 20:54:25 +0500

    Реализован словарь ресурсов. Примерная структура курсового проекта

--- Курсовой проект_Тепляков/MainWindow.xaml.cs
+++ Курсовой проект_Тепляков/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +21,30 @@ namespace Курсовой_проект_Тепляков
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static Pages.Login_Regin.Login login;
         public MainWindow()
         {
             InitializeComponent();
+            login = new Pages.Login_Regin.Login();
+            OpenPageLogin();
+        }
+
+        public void OpenPageLogin()
+        {
+            DoubleAnimation opgridAnimation = new DoubleAnimation();
+            opgridAnimation.From = 1;
+            opgridAnimation.To = 0;
+            opgridAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            opgridAnimation.Completed += delegate
+            {
+                frame.Navigate(login);
+                DoubleAnimation opgrisdAnimation = new DoubleAnimation();
+                opgrisdAnimation.From = 0;
+                opgrisdAnimation.To = 1;
+                opgrisdAnimation.Duration = TimeSpan.FromSeconds(1.2);
+                frame.BeginAnimation(Frame.OpacityProperty, opgrisdAnimation);
+            };
+            frame.BeginAnimation(Frame.OpacityProperty, opgridAnimation);
         }
     }
 }
