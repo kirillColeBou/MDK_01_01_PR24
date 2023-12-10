ffb3aa753292478bdecac28d5bae3f6ce30617c6
Kirill <kirillteplycov888@gmail.com>
2023-12-10 15:00:59 +0500

    Изменено событие ошибки при регистрации, теперь правильно и красивее

--- Курсовой проект_Тепляков/Pages/Login_Regin/Regin.xaml.cs
+++ Курсовой проект_Тепляков/Pages/Login_Regin/Regin.xaml.cs
@@ -31,17 +31,50 @@ namespace Курсовой_проект_Тепляков.Pages.Login_Regin
             MainWindow.main.OpenPageLogin();
         }
 
+        private void login_user_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            login_incorrect.Visibility = Visibility.Hidden;
+            login_new_user.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3D3D3D"));
+        }
+
+        private void password_user_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            password_incorrect_first.Visibility = Visibility.Hidden;
+            password_new_user_first.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3D3D3D"));
+            password_incorrect_second.Visibility = Visibility.Hidden;
+            password_new_user_second.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3D3D3D"));
+        }
+
         private void Login_Click(object sender, MouseButtonEventArgs e)
         {
+            password_incorrect_first.Content = "Пароли не совпадают";
+            password_incorrect_first.Visibility = Visibility.Hidden;
+            password_incorrect_second.Content = "Пароли не совпадают";
+            password_incorrect_second.Visibility = Visibility.Hidden;
             if (login_new_user.Text != "" && password_new_user_first.Password != "" && password_new_user_second.Password != "")
                 if (password_new_user_first.Password == password_new_user_second.Password)
                 {
                     MainWindow.init.OpenPageMain();
-                }  
+                }
                 else
-                    MessageBox.Show("Пароли не совпадают");
+                {
+                    password_new_user_first.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB3F51"));
+                    password_incorrect_first.Visibility = Visibility.Visible;
+                    password_new_user_second.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB3F51"));
+                    password_incorrect_second.Visibility = Visibility.Visible;
+                }
             else
-                MessageBox.Show("Введите данные нового пользователя");
+            {
+                login_new_user.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB3F51"));
+                login_incorrect.Content = "Введите логин";
+                login_incorrect.Visibility = Visibility.Visible;
+                password_new_user_first.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB3F51"));
+                password_incorrect_first.Content = "Введите пароль";
+                password_incorrect_first.Visibility = Visibility.Visible;
+                password_new_user_second.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FB3F51"));
+                password_incorrect_second.Content = "Введите пароль";
+                password_incorrect_second.Visibility = Visibility.Visible;
+            }
         }
     }
 }
